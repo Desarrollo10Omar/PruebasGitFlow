445632dde6944f627aa638d5a0ecf44c5fb679f7
LuisAbrahamSanabria <desarrollo.net.10@educem.mx>
2022-06-30 16:22:11 -0500

    Se borró una dependencia sin utilizar

--- src/main/java/com/example/demo/controller/TareaController.java
+++ src/main/java/com/example/demo/controller/TareaController.java
@@ -1,16 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Tarea;
+import com.example.demo.repository.TareaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import com.example.demo.model.Tarea;
-import com.example.demo.repository.TareaRepository;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Optional;
 
