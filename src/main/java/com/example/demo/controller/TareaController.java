0d1b73c2127506812a4f02fb58d782f572fe46b5
LuisAbrahamSanabria <desarrollo.net.10@educem.mx>
2022-06-30 13:57:26 -0500

    Funcionalidad para crear tareas

--- src/main/java/com/example/demo/controller/TareaController.java
+++ src/main/java/com/example/demo/controller/TareaController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.example.demo.model.Tarea;
 import com.example.demo.repository.TareaRepository;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TareaController {
@@ -13,10 +16,23 @@ public class TareaController {
     @Autowired
     private TareaRepository repository;
 
-    @GetMapping({"/todo","/"})
+    @GetMapping({"/tareas","/"})
     public String mostrarIndex(Model modelo){
         modelo.addAttribute("tareas", repository.findAll());
         return "index";
     }
 
+    @GetMapping("/tareas/agregar")
+    public String formularioAgregar(Model modelo) {
+        Tarea tarea = new Tarea();
+        modelo.addAttribute("tarea", tarea);
+        return "agregarTarea";
+    }
+
+    @PostMapping("/tareas")
+    public String guardarTarea(@ModelAttribute("tarea") Tarea tarea) {
+        repository.save(tarea);
+        return "redirect:/tareas";
+    }
+    
 }
